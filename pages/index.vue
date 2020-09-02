c26f620f72c1371036df5337cf7383ea306772f0
manju3 <manja.js@outlook.com>
2020-09-02 14:17:42 +0530

    update

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout wrap>
+    <v-flex xs12>
+      <v-card>
+        <v-card-text>
+          <h1>Welcome</h1>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  name: "index",
+  data: () => ({}),
+};
+</script>
